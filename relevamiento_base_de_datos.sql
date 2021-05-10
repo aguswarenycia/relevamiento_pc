18c76d2c35bd315d3868489cb478ea8e0887f26e
aguswarenycia <73973129+aguswarenycia@users.noreply.github.com>
2021-05-10 17:48:55 -0300

    Base de datos con las claves cambiadas

---
+++ relevamiento_base_de_datos.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2021 a las 22:47:38
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `relevamiento`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `computadoras`
+--
+
+CREATE TABLE `computadoras` (
+  `nombre` varchar(250) NOT NULL,
+  `ip` varchar(250) NOT NULL,
+  `arquitectura_so` varchar(250) NOT NULL,
+  `tipo_maquina` varchar(250) NOT NULL,
+  `procesador` varchar(250) NOT NULL,
+  `cores_fisicos` int(11) NOT NULL,
+  `cores_totales` int(11) NOT NULL,
+  `ram_total` varchar(250) NOT NULL,
+  `ram_usada` varchar(250) NOT NULL,
+  `ram_disponible` varchar(250) NOT NULL,
+  `espacio_tot_c` varchar(250) NOT NULL,
+  `espacio_usado_c` varchar(250) NOT NULL,
+  `espacio_disponible_c` varchar(250) NOT NULL,
+  `espacio_tot_d` varchar(250) NOT NULL,
+  `espacio_usado_d` varchar(250) NOT NULL,
+  `espacio_disponible_d` varchar(250) NOT NULL,
+  `anydesk` varchar(10) NOT NULL,
+  `id_anydesk` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `farmacias`
+--
+
+CREATE TABLE `farmacias` (
+  `id_farmacia` varchar(100) NOT NULL,
+  `nombreFcia` varchar(250) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `ip` varchar(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `apellido` varchar(250) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(50) NOT NULL,
+  `perfil_id` int(11) NOT NULL,
+  `baja` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `computadoras`
+--
+ALTER TABLE `computadoras`
+  ADD PRIMARY KEY (`ip`);
+
+--
+-- Indices de la tabla `farmacias`
+--
+ALTER TABLE `farmacias`
+  ADD PRIMARY KEY (`id_farmacia`),
+  ADD KEY `ip` (`ip`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `perfil_id` (`perfil_id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`perfil_id`) REFERENCES `perfil` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
