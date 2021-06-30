6eb5e889cdd153e1dba93953b5b548935d1cb234
Agustina Warenycia <simstamy1@gmail.com>
2021-06-30 16:32:13 -0300

    Ya anda la lista de farmacia, si funciona 4k 1 link mega

---
+++ Documents/Proyecto2.0/relevamiento/farmacia/urls.py
@@ -0,0 +1,50 @@
+# es necesario el url patterns  #URL CRUD
+#from relevamiento.farmacia.models import Provincia
+from django.urls import path
+
+from django.contrib.auth.decorators import login_required
+# from django.urls import reverse_lazy
+from .views import (
+    ProbandoLista,
+    AgregarLoc,
+    ListarLocDes,
+    #ActualizarProvAct,
+    ActivarProvincia,
+    ActivarPrograma,
+    ListarProvDesactivadas,
+    ListarProgramas,
+    AgregarPrograma,
+    EliminarPrograma,
+    Actualizarprograma,
+    EliminarProv,
+    ActualizarProv,
+    CrearProv, 
+    ListarProv,  
+    ListarFcias,   
+    ListarLoc,
+    ListarProgDesactivados
+)
+
+urlpatterns = [
+    #-------------------------------- LOGIN --------------------------------
+    path('lista_localidades/', login_required(ListarLoc.as_view()), name ="lista_localidades"),
+    path('agregar_localidad/',login_required(AgregarLoc.as_view()),name = "agregar_localidad"),
+    #-------------------------- Ruteo de CRUD  Para Programas --------------------------
+    path('lista_programas/',login_required(ListarProgramas.as_view()), name = 'lista_programas'),
+    path('agregar_programas/',login_required(AgregarPrograma.as_view()), name = 'agregar_programas'),
+    path('eliminar_programa/<int:pk>',login_required(EliminarPrograma.as_view()), name  = 'eliminar_programa'),
+    path('actualizar_programa/<int:pk>',login_required(Actualizarprograma.as_view()), name  = 'actualizar_programa'),
+    path('activar_programa/<int:pk>', login_required(ActivarPrograma.as_view()),name = 'activar_programa'),
+
+    path('activar_provincia/<int:pk>',login_required(ActivarProvincia.as_view()),name  = 'activar_provincia'),
+    path('listar_provincias/', login_required(ListarProv.as_view()), name  = 'listar_provincias'),
+    path('lista_prov_desactivadas/',login_required(ListarProvDesactivadas.as_view()),name = 'lista_prov_desactivadas'),
+    path('agregar_prov/', CrearProv.as_view(), name  = 'agregar_prov'),
+    path('eliminar_prov/<int:pk>', EliminarProv.as_view(), name  = 'eliminar_prov'),
+    path('actualizar_prov/<int:pk>', ActualizarProv.as_view(), name  = 'actualizar_prov'),
+    path('lista_farmacias/', login_required(ListarFcias.as_view()), name ="lista_farmacias"),
+    path('lista_programas_desactivados/', login_required(ListarProgDesactivados.as_view()), name ="lista_programas_desactivados"),
+    path('localidades_desactivadas/',login_required(ListarLocDes.as_view()),name = 'localidades_desactivadas'),
+
+    path('listar_farmacias_2/',login_required(ProbandoLista.as_view()), name = 'listar_farmacias_2'),
+] 
---
+++ Documents/Proyecto2.0/relevamiento/farmacia/views.py
@@ -0,0 +1,183 @@
+#from relevamiento.farmacia.models import Farmacia
+from django.views.generic.edit import CreateView
+from .forms import LocalidadForm, ProgramaForm, ProvinciaForm, ProgramaActForm, ProvinciaActForm
+from .models import Farmacia, Fcia, Programa, Provincia, Localidad
+ #import de las vistas basadas en clases
+from django.views.generic import ( 
+                                TemplateView, #Vista basada en clase para renderizar una página estática simple 
+                                ListView, #Vista basada en clase para renderizar un template de listas
+                                UpdateView, #Vista basada en clase para renderizar un template de Actualizacón
+                                CreateView, #Vista basada en clase para renderizar un template de Creacion
+                                DeleteView) #Vista basada en clase para renderizar un template de Borrado
+from django.urls import reverse_lazy
+
+# Vista basada en clase para renderizar un template simple
+class Inicio(TemplateView):
+    template_name = 'index.html'
+
+# Vista que renderiza el login
+class Login(TemplateView):
+    template_name = 'farmacia/login.html' # indicar la ruta desde la raiz sin especificar esta misma
+
+#-------------------------- CRUD de ciudades --------------------------
+
+#------------------vista para listar las localidades
+class ListarLoc(ListView):
+    model = Localidad
+    template_name = 'farmacia/listar_localidades.html'
+    context_object_name = 'localidad'
+    
+    # Redefinicion del metodo get_queryset para realizar la consulta de filtro de ciudad por provincia
+    def get_queryset(self):
+        qs = Localidad.objects.all() # qs igual
+        provincia = self.request.GET.get("lang")
+        if provincia:
+            qs = qs.filter(id_provincia_id = provincia)
+        return qs
+
+
+# Vista basada en clase para listar los datos de las localidades desactivadas
+class ListarLocDes(ListView):
+    model = Localidad
+    template_name = 'farmacia/localidades_desactivadas.html'
+    context_object_name = 'localidad'
+    queryset = Localidad.objects.all()
+
+class AgregarLoc(CreateView):
+    model = Localidad
+    template_name = 'farmacia/agregar_localidad.html'
+    form_class = LocalidadForm
+    success_url = reverse_lazy('farmacia:lista_localidades') 
+
+
+#--------------------------FIN CRUD de ciudades --------------------------
+
+#-------------------------- CRUD de programas --------------------------
+
+# Vista basada en clase para listar los programas eliminados/desactivados
+class ListarProgDesactivados(ListView):
+    model = Programa
+    template_name = 'farmacia/lista_programas_desactivados.html'
+    context_object_name = 'programas'
+    queryset = Programa.objects.all()
+
+# Vista basada en clase para listar programas (EN PROCESO)
+class ListarProgramas(ListView):
+    model = Programa
+    template_name = 'farmacia/lista_programas.html'
+    context_object_name = 'programas'
+    queryset = Programa.objects.all() # esta funcion devuelve una consulta que retorna todas las filas de la tabla programa
+
+# Vista basada en clase para agregar un nuevo programa
+class AgregarPrograma(CreateView):
+    model = Programa
+    template_name = 'farmacia/agregar_programa.html'
+    form_class = ProgramaForm
+    success_url = reverse_lazy('farmacia:lista_programas')
+
+# Vista basada en clase para eliminar completamente un objeto de la base de datos 
+class EliminarPrograma(DeleteView):
+    model = Programa
+    success_url =  reverse_lazy('farmacia:lista_programas_desactivados')
+
+#Vista basada en clase para actualizar un programa
+class Actualizarprograma(UpdateView):
+
+    model = Programa
+    template_name = 'farmacia/agregar_programa.html'
+    form_class = ProgramaForm
+    success_url = reverse_lazy('farmacia:lista_programas')
+
+class ActivarPrograma(UpdateView):
+    model = Programa
+    template_name = 'farmacia/agregar_programa.html'
+    form_class = ProgramaActForm
+    success_url = reverse_lazy('farmacia:lista_programas')
+#-------------------------- FIN CRUD de programas --------------------------
+    
+#--------------------------  CRUD de Provincias --------------------------
+
+class ActivarProvincia(UpdateView):
+    model = Provincia
+    template_name = 'farmacia/agregar_prov.html'
+    form_class = ProvinciaActForm
+    success_url = reverse_lazy('farmacia:listar_provincias')
+
+# Vista basada en clase para listar los datos de las Provincias
+class ListarProv(ListView):
+    model = Provincia
+    template_name = 'farmacia/listar_provincias.html'
+    context_object_name = 'provincias' #objeto para recorrer la tabla
+    queryset = Provincia.objects.all()
+
+
+
+
+#Vista basada en clase para listar las Provincias Desactivadas
+class ListarProvDesactivadas(ListView):
+    model = Provincia
+    template_name = 'farmacia/lista_prov_desactivadas.html'
+    context_object_name = 'provincias'
+    queryset = Provincia.objects.all() #consulta a la abse de datos
+
+# Vista basada en clase para agregar una provincia
+class CrearProv(CreateView):
+    model = Provincia
+    template_name = 'farmacia/agregar_prov.html'
+    form_class = ProvinciaForm
+    success_url = reverse_lazy('farmacia:listar_provincias')
+
+#Vista basada en clase para actualizar una provincia
+class ActualizarProv(UpdateView):
+    model = Provincia
+    template_name = 'farmacia/agregar_prov.html'
+    form_class = ProvinciaForm
+    success_url = reverse_lazy('farmacia:listar_provincias')
+
+# vista basada en clase que  hace una eliminación directa de los registros de la base de datos
+class EliminarProv(DeleteView):
+    model = Provincia
+    success_url = (reverse_lazy('farmacia:listar_provincias'))   
+
+# Codigo para editar una provincia (repetido revisar)
+class EditarProvincia(UpdateView):  
+    model = Provincia
+    template_name = 'farmacia/agregar_prov.html'
+    form_class = ProvinciaForm
+    success_url = reverse_lazy('farmacia:listar_provincias')
+
+#-------------------------- FIN CRUD de Provincias --------------------------
+
+
+
+
+# Vista basada en clase para listar las farmacias
+class ListarFcias(ListView):
+    model = Fcia
+    template_name = 'farmacia/listar_farmacias.html'
+    context_object_name = 'fcias'
+
+    # Redefinicion del metodo get_queryset para realizar la consulta de filtro de farmacia por ciudad
+    def get_queryset(self):
+        qs = Fcia.objects.all() # trago todas las farmacias
+        farmacia = self.request.GET.get("lang")
+        #Cambair esta parte del codgo para que compare el id de provincia con el de localidad y desp con el de farmacia
+        if farmacia:
+            qs = qs.filter(id_localidad = farmacia)
+        return qs
+
+
+
+
+#----------------Probando------------
+
+class ProbandoLista(ListView):
+    template_name = 'farmacia/listar_farmacia_2.html'
+    model = Provincia
+    second_model = Localidad
+    third_model = Farmacia
+    def get_context_data(self, *args, **kwargs): 
+        provincias = Provincia.objects.all()
+        localidades = Localidad.objects.all()
+        farmacias = Fcia.objects.all()
+        return {'provincias': provincias, 'localidades': localidades, 'farmacias':farmacias}
